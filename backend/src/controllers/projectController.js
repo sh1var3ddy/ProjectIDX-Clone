8c32918182f25807925f7b90572d57a0d20d78a6
sh1var3ddy <srajamuri@gmail.com>
2025-12-18 18:40:46 -0500

    Updated react project command

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -2,7 +2,7 @@ import util from "util";
 import child_process from "child_process";
 import fs from "fs/promises";
 import uuid4 from "uuid4";
-
+import { REACT_PROJECT_COMMAND } from "../config/serverConfig.js";
 
 const execPromisified = util.promisify(child_process.exec);
 export const  createProjectController = async (req,res)=>{
@@ -13,7 +13,7 @@ export const  createProjectController = async (req,res)=>{
     await fs.mkdir(`./projects/${projectId}`);
 
     // After this call the npm create vite command in the newly created project
-    const response = await execPromisified(`npm create vite@latest sandbox -- --template react`,{
+    const response = await execPromisified(REACT_PROJECT_COMMAND,{
         cwd:`./projects/${projectId}`
     });
 
